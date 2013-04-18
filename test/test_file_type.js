266a47b2bfc9a46ea8c61fbf66f52607b11b8848
Andrew Kelley <superjoe30@gmail.com>
2013-04-18 12:04:14 -0400

    add failing file_type tests

---
+++ test/test_file_type.js
@@ -0,0 +1,28 @@
+var nodagen = require('../')
+  , path = require('path')
+  , assert = require('assert')
+  , data = path.join(__dirname, "data")
+  , FileType = nodagen.FileType
+  , file = nodagen.file
+
+describe("FileType", function() {
+  var vorbis;
+  before(function() {
+    vorbis = file(path.join(data, "empty.ogg"));
+  });
+  it("delitem_not_there", function() {
+    assert.throws(function() {
+      vorbis.delItem("foobar");
+    }, /KeyError/);
+  });
+  it("add_tags", function() {
+    assert.throws(function() {
+      var x = (new FileType()).add_tags();
+    }, /Not implemented/);
+  });
+  it("delitem", function() {
+    vorbis.setItem("foobar", "quux");
+    vorbis.delItem("foobar");
+    assert.strictEqual(vorbis.values().indexOf("quux"), -1);
+  });
+});
